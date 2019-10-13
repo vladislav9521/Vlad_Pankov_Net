5924675131054c8ddc7574522304b485a586b6be
vladislav9521 <55600833+vladislav9521@users.noreply.github.com>
2019-10-13 09:45:53 +0300

    Модуль 2 - задание 1

---
+++ Module2_1/Module2_1/Module2_1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Module2_1
+{
+
+    //1)	Система подсчета налогов.Пользователь вводит N – число комапаний, работающих в стране,
+    //    а так же M% – налог, который взымает государство.Если представить, что доход, который
+    //    получат всё эти компании одинаковый и он равен 500, то какой суммарный налог 
+    //    государство получит?
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int countComp;
+            double taxSum, incOfOne = 500.0, taxPC;
+
+            Console.Write("Введите число компаний: ");
+            countComp = Convert.ToInt16(Console.ReadLine());
+            Console.Write("Введите налог(в %): ");
+            taxPC = Convert.ToDouble(Console.ReadLine());
+            
+            taxSum = (incOfOne * taxPC / 100) * countComp;
+
+            Console.WriteLine($"Суммарный налог государству {countComp} компаний\n" +
+                              $"при доходе каждой в  {incOfOne} у.е. составляет {taxSum} у.е.");
+            Console.ReadKey();
+        }
+    }
+}
