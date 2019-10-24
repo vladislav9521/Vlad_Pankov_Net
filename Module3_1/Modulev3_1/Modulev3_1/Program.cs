6f7751a1fc4be33e1ae25691e055bf34182c184f
vladislav9521 <55600833+vladislav9521@users.noreply.github.com>
2019-10-24 21:54:15 +0300

    Задание 1 - модуль 3

---
+++ Module3_1/Modulev3_1/Modulev3_1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+//1)	Напишите программу, которая вводит два целых числа и находит их произведение, 
+//    не используя операцию умножения.Учтите, что числа могут быть отрицательными.
+
+namespace Module3_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberA, numberB, mult = default(int);
+
+            Console.Write("Введите первое целое число: ");
+
+            while (!int.TryParse(Console.ReadLine(), out numberA))
+            {
+                Console.Write("Некорректный ввод!!! Введите первое целое число: ");
+            }
+
+            Console.Write("Введте второе целое число: ");
+
+            while (!int.TryParse(Console.ReadLine(), out numberB))
+            {
+                Console.Write("Некорректный ввод!!! Введите второе целое число: ");
+            }
+
+            if (numberA == 0 || numberB == 0)
+            {
+                mult = 0;
+            }
+            else
+            {
+                for (int i = 1; i <= Math.Abs(numberB); i++)
+                {
+                    mult += Math.Abs(numberA);
+                }
+                mult = ((numberA < 0) && (numberB > 0)) || ((numberA > 0) && (numberB < 0)) ? (~mult + 1) : mult;
+            }
+
+            Console.WriteLine($"Произведение чисел {numberA} и {numberB} = {mult}");
+            Console.ReadKey();
+        }
+    }
+}
