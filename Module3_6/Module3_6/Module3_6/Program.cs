83abd326541455da12313a72916e5cf55611d59c
vladislav9521 <55600833+vladislav9521@users.noreply.github.com>
2019-10-24 22:17:10 +0300

    Задание 6 - модуль 3

---
+++ Module3_6/Module3_6/Module3_6/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Module3_6
+{
+
+    // 6) Дан массив, содержащий положительные и отрицательные числа.
+    // Заменить все элементы массива на противоположные по знаку
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите количество элементов массива: ");
+
+            uint count;
+
+            while (!uint.TryParse(Console.ReadLine(), out count) || count == 0)
+            {
+                Console.Write("Некорректный ввод!!! Введите количество элементов масива: ");
+            }
+
+            Console.Write("Массив до преобразования: ");
+
+            double[] numbers = new double[count];
+            Random rand = new Random();
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = rand.Next(-100, 100) / 10.0;
+                Console.Write(numbers[i] + "\t");
+            }
+
+            Console.Write("\nМассив после преобразования: ");
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] *= -1;
+                Console.Write(numbers[i] + "\t");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
